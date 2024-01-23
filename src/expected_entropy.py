c5e57eb3bfd839057d7274c4261382bf468eb88a
Elia Mascolo <eliamascolo94@gmail.com>
2024-01-23 12:51:33 -0500

    [FEAT] Add code (not used) for the general computation of the expected value of Shannon's Entropy.

--- src/expected_entropy.py
+++ src/expected_entropy.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from scipy.stats import multinomial
+import math
 
 
 def entropy(counts):
@@ -34,9 +35,9 @@ def expected_entropy(n, base_probabilities=[0.25, 0.25, 0.25, 0.25]):
     Parameters
     ----------
     n : int
-        DESCRIPTION.
+        Sample size.
     base_probabilities : array-like, optional
-        DESCRIPTION. The default is [0.25, 0.25, 0.25, 0.25].
+        Probability of each base. The default is [0.25, 0.25, 0.25, 0.25].
 
     Returns
     -------
@@ -90,6 +91,117 @@ def expected_entropy(n, base_probabilities=[0.25, 0.25, 0.25, 0.25]):
                 na -= 1
                 nc += 1
     return E
+
+
+
+
+# ===================================================================
+# Expected value of Entropy for Discrete Uniform Sample of size gamma
+# ===================================================================
+
+
+
+def append_partitions(out_list, n, *rest):
+    '''
+    Appends all the possible partitions of `n` to a pre-existing list `out_list`.
+    Every partition is itself a list.
+    '''
+    out_list.append([n, *rest])
+    min = rest[0] if rest else 1
+    max = n // 2
+    for i in range(min, max+1):
+        append_partitions(out_list, n-i, i, *rest)
+
+
+def count_distributions(partition, N_bins):
+    '''
+    Counts how many distributions correspond to a specified partition of the
+    total number of elements T, where T is sum(partition).
+    `partition` is assumed to be a vector of non-zero counts. The non-zero counts
+    in `partition` refer to one of many possible bins. The total number of
+    possible bins is `N_bins`.
+    
+    Example:
+        N_bins = 8
+        partition = [2,2,1]
+        T = 5 (because 2+2+1=5)
+        
+        This function will return 168, because there are 168 distributions of
+        five counts over 8 bins such that two bins contain 2 counts, one bin
+        contains 1 count and all the ramaining five bins contain 0 counts.
+        
+        One of those 168 distributions would be:
+            [0,0,2,1,0,2,0,0]
+    '''
+    # Non-empty bins
+    m = len(partition)
+    
+    # The number of ways to map `m` non-empty bins onto `N_bins` possible bins is:
+    # N_bins!/(N_bins-m)!  To avoid factorials (big nubers) I compute the ratio directly
+    num = math.prod([i for i in range(N_bins, N_bins-m, -1)])
+    
+    # Duplicates: avoid counting multiple times the same distribution (some elements
+    # are not unique).
+    # Example: the third and sixth bins of [0,0,2,1,0,2,0,0] can be swapped
+    den = 1
+    for count_val in list(set(partition)):
+        if partition.count(count_val) > 1:
+            den *= math.factorial(partition.count(count_val))
+    return int(num/den)
+
+
+def prob_of_distribution(distribution, N_bins):
+    '''
+    Probability of obtaining the exact list of counts as in `distribution`,
+    given that bins are equiprobable, and given that there were `N_bins` bins.
+    '''
+    n_empty_bins = N_bins - len(distribution)
+    distribution = distribution + [0]*n_empty_bins  # Add empty bins
+    bin_probs = [1/N_bins] * N_bins  # Discrete uniform over the bins
+    return multinomial.pmf(distribution, n=sum(distribution), p=bin_probs)
+    
+
+def prob_of_partition(partition, N_bins):
+    '''
+    Returns the probability that T elements will fall into bins in a way that
+    partitions T into a set of counts as `partition`. T is sum(partition).
+    
+    Example:
+        N_bins = 8
+        partition = [2,2,1]
+        T = 5 (because 2+2+1=5)
+        
+        This function will return ~ 0.1538, which is the probability that, after
+        randomly placing 5 elements in 8 bins, two bins will contain two elements,
+        one bin will contain one element, and the remaining bins will be empty.
+    '''
+    return count_distributions(partition, N_bins) * prob_of_distribution(partition, N_bins)
+
+
+def exp_entropy(n, N_bins):
+    # All the partitions of n
+    partitions = []
+    append_partitions(partitions, n)
+    # Expected value of Shannon Entropy (H)
+    E = 0
+    tot_p = 0
+    for partition in partitions:
+        if len(partition) <= N_bins:
+            p = prob_of_partition(partition, N_bins)
+            H = entropy(partition + [0]*(N_bins-len(partition)))
+            E += p*H
+            tot_p += p
+    return E / tot_p
+
+
+
+
+
+
+
+
+
+
 
 
 
